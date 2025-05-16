c50e9fb4389cc1aea887c25068d20000f6afa96e
sriharshamoparthy <sriharshamoparthy@gmail.com>
2025-05-16 18:01:03 +0530

    Upload

---
+++ src/predict.py
@@ -0,0 +1,19 @@
+from src.preprocessing import DataProcessing
+import pickle
+
+def predict(X):
+    # Load the model and scaler from the saved file
+    with open("model/model.pickle", 'rb') as f:
+        print("Model Imported")
+        model, label_encoders, scaler = pickle.load(f)
+    dataprocess = DataProcessing()
+    dataprocess.cleaning_steps(X)               # Perform Cleaning
+    dataprocess.perform_feature_engineering(X)  # Perform Feature Engineering
+
+    # Label Encoding
+    for column, label_encoder in label_encoders.items():
+        X[column] = label_encoder.transform(X[column])
+
+    X = scaler.transform(X)  # Standardize
+    pred = model.predict(X)  # Predict time of delivery
+    return pred
